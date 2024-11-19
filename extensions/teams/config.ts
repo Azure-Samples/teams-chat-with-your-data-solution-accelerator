d0717891b6def2a4535809bb8e20f2032ced7ada
John Metzger <56820659+jometzg@users.noreply.github.com>
2024-11-19 11:29:25 +0000

    Cosmos chat persistence

--- extensions/teams/config.ts
+++ extensions/teams/config.ts
@@ -2,6 +2,10 @@ const config = {
   botId: process.env.BOT_ID,
   botPassword: process.env.BOT_PASSWORD,
   azureFunctionUrl: process.env.AZURE_FUNCTION_URL,
+  cosmosEndpoint: process.env.COSMOSDB_ENDPOINT,
+  cosmosPassword: process.env.COSMOSDB_KEY,
+  cosmosDatabaseName: process.env.COSMOSDB_DATABASE_NAME,
+  cosmosContainerName: process.env.COSMOSDB_CONTAINER_NAME
 };
 
 export default config;
